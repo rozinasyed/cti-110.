5d8bf65b0eb6835016e717160c4ac1a853f39ec4
rozinasyed <syedr2547@student.faytechcc.edu>
2017-06-20 15:15:27 -0600

    cti 110
    variables.py

---
+++ variables.py
@@ -0,0 +1,44 @@
+# Variable
+#simple variable
+
+name = "Rehman"
+age = 5
+salary = 10000.0
+
+  
+print(name)
+print(age)
+print(salary)
+
+ #Multipal variable in one line
+
+Ename, Eage, Esalary = "Rehman", 5, 1000.0
+print(Ename)
+print (Eage)
+print (Esalary)
+
+
+# multivariable assign single value
+rehman = mani = mano = "Blue"
+print(rehman)
+print(mani)
+print(mano)
+
+room =503
+print("i am staying in this room")
+print(503)
+
+
+
+#two variables:top_speed and distance.
+
+top_speed =160
+distance = 300
+
+#Display the values referenced by the variable.
+
+print("The top speed is")
+print(top_speed)
+print("The distance travele is")
+print(distance)
+      
