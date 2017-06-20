8eacc0233d6677dd90131c196e9c8bf6047ac295
rozinasyed <syedr2547@student.faytechcc.edu>
2017-06-20 15:12:31 -0600

    cti 110
    M2HW1_DistanceTraveled_Syed.py.py

---
+++ M2HW1_DistanceTraveled_Syed.py.py
@@ -0,0 +1,12 @@
+
+# Distance Traveled
+# 6/9/17
+# CTI-110 M2HW1 - Distance Traveled 
+# Syed
+
+#Distance  speed * Time
+speed = 70
+
+print( "In 6 hours, the car will travel " + str( ( speed * 6 ) ) + " miles" )
+print( "In 10 hours, the car will travel " + str( ( speed * 10 ) ) + " miles" )
+print( "In 15 hours, the car will travel " + str( ( speed * 15 ) ) + " miles" )
